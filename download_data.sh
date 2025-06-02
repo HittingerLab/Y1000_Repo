4b3af31462711bf669f524f8b5f58ae6c50cdfaa
HittingerLab <HittingerLabTemp@gmail.com>
2025-06-02 11:52:07 -0500

    updating download link

--- download_data.sh
+++ download_data.sh
@@ -2,7 +2,6 @@
 
 #Automatically downloads and unpacks Y1000+ Genomes and attations
 
-#Data can be accessed directly at https://data.glbrc.org/shared_collections/321/download_all?ak=a9a366fec2223e1a11a6f6a401cae257
+#Data can be accessed directly at https://data.glbrc.org/shared_collections/341/download_all?ak=8c2aa328e739ae78d0c08e8711b98a0e
 
-wget -O Y1000dataV1.0.zip https://data.glbrc.org/shared_collections/321/download_all?ak=a9a366fec2223e1a11a6f6a401cae257
-
+wget -O Y1000dataV1.0.zip https://data.glbrc.org/shared_collections/341/download_all?ak=8c2aa328e739ae78d0c08e8711b98a0e
